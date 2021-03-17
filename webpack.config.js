60d9d87e145435cc77c643fc56bb64000a1b01ce
MuriloDutra <MuriloDutraProjects@gmail.com>
2021-03-17 10:33:44 -0300

    Asset Management module - DONE

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,69 @@
 const path = require('path')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const toml = require('toml')
+const yaml = require('yamljs')
+const json5 = require('json5')
+
 
 module.exports = {
     mode: 'production',
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                use: ['url-loader?limit=100000']
+            },
+            {
+                test: /\.(csv|tsv)$/i,
+                use: ['csv-loader'],
+            },
+            {
+                test: /\.xml$/i,
+                use: ['xml-loader'],
+            },
+            {
+                test: /\.toml$/i,
+                type: 'json',
+                parser: {
+                    parse: toml.parse,
+                }
+            },
+            {
+                test: /\.yaml$/i,
+                type: 'json',
+                parser: {
+                    parse: yaml.parse,
+                }
+            },
+            {
+                test: /\.json5$/i,
+                type: 'json',
+                parser: {
+                    parse: json5.parse,
+                }
+            }
+        ]
+    },
+    optimization:{
+        minimize: true,
+        minimizer: [
+            `...`, //syntax to extend existing minimizers
+            new CssMinimizerPlugin({
+                test: /\.css$/i,
+            }),
+        ]
+    },
 }
