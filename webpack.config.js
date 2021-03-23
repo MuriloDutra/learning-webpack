b1c7bcecd29a0ad3fa63334a1fca8ad83d43f7a0
MuriloDutra <MuriloDutraProjects@gmail.com>
2021-03-23 11:52:22 -0300

    Code splitting - Entry Points DONE

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,16 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     mode: 'development',
     entry: {
-        index:'./src/index.js',
+        index:{
+           import: './src/index.js',
+           dependOn: 'shared',
+        },
+        another: {
+            import: './src/another-module.js',
+            dependOn: 'shared'
+        },
+        shared: 'lodash',
         print: './src/print.js',
-        another: './src/another-module.js',
     },
     devtool: 'inline-source-map',
     devServer: {
@@ -24,4 +31,7 @@ module.exports = {
         clean: true, //It cleans '/dist' folder before a new build
         publicPath: '/', //The publicPath will be used within our server script as well in order to make sure files are served correctly on http://localhost:3000. The next step is setting up our custom express server
     },
+    optimization: {
+        runtimeChunk: 'single'
+    }
 }
